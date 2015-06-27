1a18519eb730c458d756963d021a6768febe85b7
Jackie Zhang <viperking@163.com>
2015-06-27 16:05:39 +0800

    adding code about registering

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var register = require('./routes/register');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+/* GET/POST register page*/
+router.get('/register', register.form);
+router.post('/register', register.submit);
 
 module.exports = router;
---
+++ routes/register.js
@@ -0,0 +1,7 @@
+/**
+ * Created by jackiezhang on 15/6/27.
+ */
+
+exports.form = function(req, res) {
+  res.render('register', {title: 'register'});
+};

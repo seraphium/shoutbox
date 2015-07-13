a677b6717fa26f4ea72d3173569b8016f1390a28
seraphium <viperking@163.com>
2015-07-13 11:06:03 +0800

    added should test framework

--- test/testcases.js
+++ test/testcases.js
@@ -1,11 +1,13 @@
 /**
  * Created by jackiezhang on 15/7/11.
  */
-
+var should = require('should');
 
 exports.testPony = function(test)
 {
     //test.expect(2);
-    test.ok(true, "this should be passed");
+    //test.ok(true, "this should be passed");
+    var xx = true;
+    xx.should.equal(false);
     test.done();
 }
